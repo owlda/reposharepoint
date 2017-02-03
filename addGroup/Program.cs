9e1f187cb3e6f091d1f405ccd546aeca0825fbef
Owl <ad-2812@yandex.com>
2017-02-03 18:31:38 +0300

    Add files via upload

---
+++ addGroup/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.SharePoint;
+
+namespace addGroupUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Name of group users
+            Console.WriteLine("-----------------------------------------------");
+            Console.Write("Enter the site address: ");
+            String SiteAddress = Console.ReadLine();
+            Console.WriteLine("You entered: {0}", SiteAddress);
+
+            // Name of group users
+            Console.WriteLine("-----------------------------------------------");
+            Console.Write("Enter the name of group: ");
+            String newGroup = Console.ReadLine();
+            Console.WriteLine("You entered: {0}", newGroup);
+
+            // Level of permission
+            Console.WriteLine("-----------------------------------------------");
+            Console.Write("Enter the level of permission: ");
+            String LevelPerm = Console.ReadLine();
+            Console.WriteLine("You entered: {0}", LevelPerm);
+
+
+            SPSite site = new SPSite(SiteAddress);
+            using (SPWeb web = site.OpenWeb())
+            {
+                String Name = newGroup;
+                web.SiteGroups.Add(Name, web.CurrentUser, web.CurrentUser,"group created by Administrator");
+
+                
+                SPGroup group = web.SiteGroups[newGroup];
+                SPRoleAssignment roles = new SPRoleAssignment(group);
+                SPRoleDefinition  permissions = web.RoleDefinitions[LevelPerm];
+                roles.RoleDefinitionBindings.Add(permissions);
+                web.RoleAssignments.Add(roles);
+
+             }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        
+        }
+    }
+}
